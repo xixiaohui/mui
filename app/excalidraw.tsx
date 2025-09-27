b6e2fa96b78da8b82077bd60254cf7e37e17de86
unknown <yamazake@yeah.net>
2025-09-27 16:26:54 +0800

    add excalidraw

--- app/excalidraw.tsx
+++ app/excalidraw.tsx
@@ -1,11 +1,6 @@
 "use client";
 
-
-
 import dynamic from "next/dynamic";
-import { useState } from "react";
-
-
 
 const Excalidraw2 = dynamic(
   async () => (await import("@excalidraw/excalidraw")).Excalidraw,
@@ -14,21 +9,9 @@ const Excalidraw2 = dynamic(
 
 export default function ExcalidrawPage(){
 
-  const [elements, setElements] = useState([]);
-  const [appState, setAppState] = useState({});
-
   return (
     <div style={{ height: "100vh" }}>
-      <Excalidraw2
-        initialData={{
-          elements: elements,
-          appState: appState,
-        }}
-        onChange={(elements, appState) => {
-          setElements(elements);
-          setAppState(appState);
-        }}
-      />
+      <Excalidraw2 />
     </div>
   );
 
